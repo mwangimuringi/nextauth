0d5f651fa7488e16c5e70914223e5e0041bfcabd
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-17 22:10:50 +0300

    Starter files added

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='font-semibold'>
+      <Button >Click me</Button>
+    </div>
+  )
+}
+
+export default Home;
