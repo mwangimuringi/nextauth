9ae9976e3a1a140427937dbc01fd31ba6529fd17
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-17 23:54:56 +0300

    HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import { Button } from '@/components/ui/button';
+import { Poppins } from "next/font/google";
+import { cn } from '@/lib/utils';
 import React from 'react';
 
-const Home = () => {
+
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
+export default function Home () {
   return (
-    <div className='font-semibold'>
-      <Button >Click me</Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          font.className,
+        )}>
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+      </div>
+    </main>
   )
 }
 
-export default Home;
