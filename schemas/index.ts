4a84723cb83c0b778ffd1affe39586667ddaec02
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-19 12:50:42 +0300

    Created Register Form

--- schemas/index.ts
+++ schemas/index.ts
@@ -52,14 +52,14 @@ export const LoginSchema = z.object({
   code: z.optional(z.string()),
 });
 
-// export const RegisterSchema = z.object({
-//   email: z.string().email({
-//     message: "Email is required",
-//   }),
-//   password: z.string().min(6, {
-//     message: "Minimum 6 characters required",
-//   }),
-//   name: z.string().min(1, {
-//     message: "Name is required",
-//   }),
-// });
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
