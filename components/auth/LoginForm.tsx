213c3e11142e98cf85957efc8dd611015e262fa9
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-20 17:35:06 +0300

    ...

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -23,6 +23,13 @@ import { FormSuccess } from "../FormSuccess";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+    ? "Email already in use with different provider!"
+    : "";
+
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -41,8 +48,8 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // setSuccess(data?.success);
       });
     });
   };
@@ -95,7 +102,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message = {error} />
+          <FormError message = {error|| urlError} />
           <FormSuccess message = {success} />
           <Button disabled={isPending} type="submit" className="w-full">
             Login
