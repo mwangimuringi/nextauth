ed408c2875e4c8187264019509814fe96a4ea4cb
mwangimuringi <mwangimuringi10@gmail.com>
2024-08-22 20:05:39 +0300

    Updating of user id in settings page

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
@@ -6,92 +6,97 @@ import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
 import { getTwoFactorConfirmationByUserId } from "./data/TwoFactorConfirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    pages: {
-        signIn: "/auth/login",
-        error: "/auth/error",
-      },
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
 
-    events: {
-        async linkAccount({ user }) {
-          await db.user.update({
-            where: { id: user.id },
-            data: { emailVerified: new Date() }
-          })
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+
+  callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== "credentials") return true;
+
+      if (!user.id) {
+        throw new Error("User ID is missing or undefined.");
+      }
+
+      const existingUser = await getUserById(user.id);
+
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) return false;
+
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
+
+      return true;
+    },
+
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        // session.user.email = token.email;
+        if (token?.email && session?.user) {
+          session.user.email = token.email;
         }
-      },
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
 
-    callbacks: {
-        async signIn({ user, account }) {
-          // Allow OAuth without email verification
-          if (account?.provider !== "credentials") return true;
-    
-          if (!user.id) {
-            throw new Error("User ID is missing or undefined.");
-          }
-          
-          const existingUser = await getUserById(user.id);
-    
-          // Prevent sign in without email verification
-          if (!existingUser?.emailVerified) return false;
-    
-          if (existingUser.isTwoFactorEnabled) {
-            const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(existingUser.id);
-    
-            if (!twoFactorConfirmation) return false;
-    
-            // Delete two factor confirmation for next sign in
-            await db.twoFactorConfirmation.delete({
-              where: { id: twoFactorConfirmation.id }
-            });
-          }
-    
-          return true;
-        },
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token; //logged out
 
-        async session({ token, session }) {
-          if (token.sub && session.user) {
-            session.user.id = token.sub;
-          }
-    
-          if (token.role && session.user) {
-            session.user.role = token.role as UserRole;
-          }
-    
-          if (session.user) {
-            session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
-          }
-    
-          // if (session.user) {
-          //   session.user.name = token.name;
-          //   session.user.email = token.email;
-          //   session.user.isOAuth = token.isOAuth as boolean;
-          // }
-    
-          return session;
-        },
-        async jwt({ token }) {
-          if (!token.sub) return token; //logged out
-    
-          const existingUser = await getUserById(token.sub);
-    
-          if (!existingUser) return token;
-    
-        //   const existingAccount = await getAccountByUserId(
-        //     existingUser.id
-        //   );
-    
-        //   token.isOAuth = !!existingAccount;
-        //   token.name = existingUser.name;
-        //   token.email = existingUser.email;
-          token.role = existingUser.role;
-        //   token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
-    
-          return token;
-        }
-      },
-    adapter: PrismaAdapter(db),
-    session: { strategy: "jwt" },
-    ...authConfig,
-})
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
+
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
